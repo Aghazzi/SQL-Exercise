5b583878a612cb47828c14dfc21f3dba20cde8f7
Aghazzi <aghazzi.1998@gmail.com>
2022-12-11 17:23:21 +0200

    Creating table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,21 @@ SELECT Points FROM students WHERE  name= "ALEX";
 INSERT INTO "main"."students"("ID","Name","Age","Gender","Points") VALUES (7,'Ahmad',24,'M',100);
 UPDATE students SET Points= Points+100 WHERE name="Basma"
 UPDATE students SET Points= Points-100 WHERE name="Alex"
+
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+
+INSERT INTO "main"."graduates" ("Name", "Age", "Gender", "Points") VALUES ('Layal', 18, 'F', 350);
+
+UPDATE "main"."graduates" SET "Graduation"="2018-09-08" WHERE "name"="Layal"
+
+DELETE FROM "main"."students" WHERE _rowid_ IN ('4');
+
+
